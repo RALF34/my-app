24e825a32f8e34a8e8fe0e45a6fc8b048ef867ce
gary <raphaello34@hotmail.fr>
2024-05-10 14:18:49 +0200

    add files

--- daily_air_quality.py
+++ daily_air_quality.py
@@ -13,40 +13,48 @@ def main():
             ### Select a place in France
             Identify the air quality monitoring station whose pollution data you are interested in.
             ''')
-    st.session_state["current_data"] = []
-    st.session_state["start"] = 90
+    
+    ending_date = date.fromisoformat(
+        open(r"data/last_update.txt", "r").read()) - timedelta(days=1)
+    st.session_state["starting_date"] = ending_date-timedelta(days=90)
+    st.session_state["current_data"] = [None, None]
     st.session_state["y-values"] = [None, None]
     st.session_state["first_choice"] = True
     st.session_state["no_data"] = True
+    
+    try:
+        def update_values() -> None:
+            counter = 0
+            for i, data in enumerate(st.session_state["current_data"]):
+                if data:
+                    # Initialize "dictionary" which will contain the average
+                    # concentration values (set to zero when no data are
+                    # available) associated to the 24 hours of the day.
+                    dictionary = {str(x): 0 for x in range(24)}
+                    for hour in data.groups.keys():
+                        # Extract only air concentration values being less than
+                        # "n_days" days old.
+                        history = list(zip(data["value"],data["date"]))[::-1]
+                        j = 0
+                        limit = len(history)
+                        while (
+                            j < limit and 
+                            (st.session_state["starting_date"] <= history[j][1])):
+                            j += 1
+                        if j == limit:
+                            j -= 1
+                        # Update "dictionary".
+                        dictionary[str(hour)] = \
+                        mean([e[0] for e in history[:j]])
+                    st.session_state["y-values"][i] = list(dictionary.values())
+                else:
+                    counter += 1
+            if counter < 2:
+                st.session_state["no_data"] = False
 
-    def update_values() -> None:
-        counter = 0
-        for i, data in enumerate(st.session_state["current_data"]):
-            if data:
-                # Initialize "dictionary" which will contain the average
-                # concentration values (set to zero when no data are
-                # available) associated to the 24 hours of the day.
-                dictionary = {str(x): 0 for x in range(24)}
-                for hour in data.groups.keys():
-                    # Extract only air concentration values being less than
-                    # "n_days" days old.
-                    history = list(zip(data["value"],data["date"]))[::-1]
-                    j = 0
-                    limit = len(history)
-                    while (
-                        j < limit and 
-                        (st.session_state["start"] <= history[j][1])):
-                        j += 1
-                    if j == limit:
-                        j -= 1
-                    # Update "dictionary".
-                    dictionary[str(hour)] = \
-                    mean([e[0] for e in history[:j]])
-                st.session_state["y-values"][i] = list(dictionary.values())
-            else:
-                counter += 1
-        if counter < 2:
-            st.session_state["no_data"] = False
+    except Exception as e:
+        st.error(e)
+        st.stop()
     
     col1, col2 = st.columns((5,1))
     with col1:
@@ -88,25 +96,21 @@ def main():
                 pollutant = pollution.split()[0]
 
                 data = queries.get_data(station, pollutant)
-                st.session_state["current_data"] = [
-                    e.groupby("hour") for e in data]
+                for i, gb in enumerate([e.groupby("hour") for e in data]):
+                    st.session_state["current_data"][i] = gb
                 update_values()
                 
                 if st.session_state["no_data"]:
                     st.error("No pollution data are available for the given period.")
                     st.stop()
                 else:
-                    last_update = open(
-                        r"data/last_update.txt","r").read()
-                    ending_date = \
-                    date.fromisoformat(last_update)-timedelta(days=1)
-                    start = st.slider(
+                    starting_date = st.slider(
                         "When does the air pollution analysis start?",
                         ending_date-timedelta(days=180),
                         ending_date,
                         ending_date-timedelta(days=90),
                         format="DD/MM/YY",
-                        key="start",
+                        key="starting_date",
                         on_change=update_values)
                     st.pyplot(
                         visualization.plot_variation(
